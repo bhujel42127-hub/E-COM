cb9fe81aa6e56c033b637ff260ce856f76287f54
bhujel42127-hub <bhujel42127@gmail.com>
2025-12-17 09:02:04 +0545

    work initial 17th dec

--- my-app/src/pages/SuperAdmin/AddProducts.tsx
+++ my-app/src/pages/SuperAdmin/AddProducts.tsx
@@ -10,6 +10,7 @@ export default function AddProduct() {
   const [form] = Form.useForm();
   const createProduct = useCreateProduct();
   const updateProduct = useUpdateProduct();
+  
   const navigate = useNavigate();
   const { id } = useParams();
   const isEdit = Boolean(id);
